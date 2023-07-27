72b01ceef4a8d6bf5268e02c7cbe6255dbda1ebd
Rubén <rubenmartinezTRABALLOS@gmail.com>
2023-07-27 11:39:51 +0200

    new lessons

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float turnSpeed;
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Without the speed, the vehicle moves 1 meter/second, now it moves 20 meters/second
         /* The method Update() does execute every frame, but that depends on the power of the computer
            so Time.deltaTime makes it executable every real time second. 
         */
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * turnSpeed);
     }
 }
